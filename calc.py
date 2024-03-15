4c685c2aa294316c8c0b371633d0dfc96f86762d
hrizeq17 <hrizeq@memphis.edu>
2024-03-15 15:46:58 -0500

    old python code

---
+++ calc.py
@@ -0,0 +1,14 @@
+class FactorialCalculator:
+    def calculate_factorial(self, n):
+        if n < 0:
+            return "Factorial is not defined for negative numbers."
+        elif n == 0:
+            return 1
+        else:
+            factorial = 1
+            for i in range(1, n + 1):
+                factorial *= i
+            return factorial
+factorial_calculator = FactorialCalculator()
+result = factorial_calculator.calculate_factorial(5)
+print("Factorial of 5:", result)
