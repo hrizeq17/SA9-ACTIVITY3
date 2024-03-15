09d1a552a7135d2d356ad85b2da6fbda1e090daa
hrizeq17 <hrizeq@memphis.edu>
2024-03-15 16:47:29 -0500

    rspec works

--- lib/fac.rb
+++ lib/fac.rb
@@ -1,21 +1,5 @@
-class FactorialCalculator
-    def calculate_factorial(n)
-      if n < 0
-        return "Factorial is not defined for negative numbers."
-      elsif n == 0
-        return 1
-      else
-        factorial = 1
-        (1..n).each do |i|
-          factorial *= i
-        end
-        return factorial
-      end
-    end
+class Calculator
+  def factorial(n)
+    (1..n).reduce(1, :*)
   end
-  
-  # Example usage:
-  factorial_calculator = FactorialCalculator.new
-  result = factorial_calculator.calculate_factorial(5)
-  puts "Factorial of 5:", result
-  
+end
--- spec/fac_spec.rb
+++ spec/fac_spec.rb
@@ -1,3 +1,17 @@
-require_relative 'factorial_calculator' 
+# spec/fac_spec.rb
+require 'spec_helper'
+require_relative '../lib/fac'
 
-  
+RSpec.describe Calculator do
+  describe "#factorial" do
+    it "calculates the factorial of 5" do
+      calculator = Calculator.new
+      expect(calculator.factorial(5)).to eq(120)
+    end
+
+    it "returns 1 for the factorial of 0" do
+      calculator = Calculator.new
+      expect(calculator.factorial(0)).to eq(1)
+    end
+  end
+end
